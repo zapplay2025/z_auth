8ec9a45398e8b6d508ca185c9f41cab8b29b4ffb
KunalPrajapat <kunalprajapati86308@gmail.com>
2025-06-19 18:16:47 +0530

    messege

--- src/main/java/com/zapplay/auth/repo/UserRepository.java
+++ src/main/java/com/zapplay/auth/repo/UserRepository.java
@@ -1,9 +1,11 @@
 package com.zapplay.auth.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.zapplay.auth.model.User;
 
 
+@Repository
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	
